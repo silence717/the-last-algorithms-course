2c0ba2cc434ff23535694a79d533180263d8eced
silence717 <yangfang2028@126.com>
2024-04-02 15:57:18 +0800

    add typescript compile

--- src/2810.ts
+++ src/2810.ts
@@ -1,8 +1,7 @@
 // https://leetcode.cn/problems/faulty-keyboard/description/?envType=daily-question&envId=2024-04-01
+
 // 你的笔记本键盘存在故障，每当你在上面输入字符 'i' 时，它会反转你所写的字符串。而输入其他字符则可以正常工作。
-//
 // 给你一个下标从 0 开始的字符串 s ，请你用故障键盘依次输入每个字符。
-//
 // 返回最终笔记本屏幕上输出的字符串。
 
 
@@ -12,7 +11,7 @@ function finalString(s: string): string {
 		.split('')
 		.reduce((acc, cur) => cur === 'i' ? acc.reverse() : acc.concat([cur]), [])
 		.join('')
-};
+}
 
-finalString('string')
-finalString('poiinter')
+console.log(finalString('string'))
+console.log(finalString('poiinter'))
